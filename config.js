9c291e2884e2b6e7dab25c83365dba0dcbeb279e
ariiezo <124276367+ariiezo@users.noreply.github.com>
2026-01-08 19:13:12 +0100

    Disable Coming Soon mode in configuration
    Changed 'comingSoon.enabled' to false to deactivate the Coming Soon page.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const config = {
     // Coming Soon Mode
     comingSoon: {
-        enabled: true, // Set to true to activate Coming Soon page
+        enabled: false, // Set to true to activate Coming Soon page
         launchDate: '2026-01-19T18:00:00', // Launch date in ISO format
         
         // Multilingual Content
@@ -233,6 +233,7 @@ const config = {
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = config;
 }
+
 
 
 
